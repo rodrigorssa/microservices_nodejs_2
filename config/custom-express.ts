6ad57a3ef2399b1ccb60fa1bc436e236ae96e300
rodrigorssa <rodrigo.rosa@pampa.compasso>
2019-03-12 10:29:57 -0300

    Adicionando documentação do projeto

--- config/custom-express.ts
+++ config/custom-express.ts
@@ -2,13 +2,17 @@ import * as express from 'express'
 import * as bodyParser from 'body-parser'
 import routes from '../src/routes/routes'
 import * as expressValidator from 'express-validator'
+import * as swaggerUi from 'swagger-ui-express'
+
+
+const swaggerDocument = require('../documentation/swagger.json')
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(expressValidator())
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 routes(app)
 

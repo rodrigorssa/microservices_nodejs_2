602cb62e204d2c9158743c00e31b6f5fe0156ce7
unknown <rodrigo.m.rosa@hotmail.com>
2020-03-23 15:40:56 -0300

    adicionando winston, morgan e arquivos do eslint

--- config/custom-express.ts
+++ config/custom-express.ts
@@ -4,6 +4,7 @@ import routes from '../src/routes/routes'
 import * as expressValidator from 'express-validator'
 import * as swaggerUi from 'swagger-ui-express'
 import { createConnection } from 'typeorm'
+import * as morgan from 'morgan'
 
 const swaggerDocument = require('../documentation/swagger.json')
 
@@ -12,10 +13,16 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(expressValidator())
+app.use(morgan('combined'))
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 createConnection().then(async () => {
     console.log('DB connection ok!');
+})
+.catch( err => {
+  console.log(err);
+  
 })
 
 routes(app)

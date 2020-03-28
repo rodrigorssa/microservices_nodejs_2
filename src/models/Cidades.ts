935dcc16db2107c346e055b6a166a75d75739e02
unknown <rodrigo.m.rosa@hotmail.com>
2020-03-28 11:00:35 -0300

    criação de dois métodos para wrapping inserção das cidades e estados

--- src/models/Cidades.ts
+++ src/models/Cidades.ts
@@ -1,19 +1,37 @@
 import { getConnection, Connection } from 'typeorm'
 import { Cidade } from '../entity/index'
+import logger from '../services/logger'
 
 export default class Cidades {
     private _connection: Connection
+    private ibgeCities: Array<Cidade>
 
     constructor () {
       this._connection = getConnection()
+      this.ibgeCities = []
     }
 
-    importarCidades (Cidade:Cidade) {
-      return this._connection.manager.save(Cidade)
-        .catch(err => {
-          console.log(err)
-          throw new Error(err)
-        })
+    async importCities (arrayOfCities: Array<Cidade>):Promise<any> {
+      try {
+        for (let index = 0; index < arrayOfCities.length; index++) {
+          await this._connection.manager.save(arrayOfCities[index])
+        }
+      } catch (err) {
+        logger.error(`Failed to save city. Error : ${err}`)
+      }
+    }
+
+    buildArrayofCities (IBGEResponse:[any]):Array<Cidade> {
+      for (let index = 0; index < IBGEResponse.length; index++) {
+        const cidadeEntity = new Cidade()
+        cidadeEntity.id = IBGEResponse[index].id
+        cidadeEntity.cidade = IBGEResponse[index].nome
+        cidadeEntity.estado = IBGEResponse[index].microrregiao.mesorregiao.UF.id
+
+        this.ibgeCities.push(cidadeEntity)
+      }
+
+      return this.ibgeCities
     }
 
     getAll () {
--- src/models/Estados.ts
+++ src/models/Estados.ts
@@ -1,27 +1,42 @@
 import { getConnection, Connection } from 'typeorm'
 import { Estado } from '../entity/index'
+import logger from '../services/logger'
 
 export default class Estados {
-    _connection: Connection
+  private _connection: Connection
+  private ibgeStates:Array<Estado>
+  constructor () {
+    this._connection = getConnection()
+    this.ibgeStates = []
+  }
 
-    constructor () {
-      this._connection = getConnection()
+  async importStates (arrayOfStates: Array<Estado>):Promise<any> {
+    try {
+      for (const state of arrayOfStates) {
+        await this._connection.manager.save(state)
+      }
+    } catch (err) {
+      logger.error(`Failed to save state. Error : ${err}`)
     }
+  }
 
-    importarEstados (Estado:Estado) {
-      this._connection.manager.save(Estado)
-        .then(res => console.log(res))
-        .catch(err => {
-          console.log(err)
-          throw new Error(err)
-        })
-    }
+  buildArrayofStates (IBGEResponse:[any]):Array<Estado> {
+    for (let index = 0; index < IBGEResponse.length; index++) {
+      const EstadoEntity = new Estado()
+      EstadoEntity.id = IBGEResponse[index].id
+      EstadoEntity.nome = IBGEResponse[index].nome
+      EstadoEntity.sigla = IBGEResponse[index].sigla
 
-    getAll () {
-      return this._connection.manager.find(Estado).catch(err => console.log(err))
+      this.ibgeStates.push(EstadoEntity)
     }
+    return this.ibgeStates
+  }
 
-    getById (id:number) {
-      return this._connection.manager.findOne(Estado, id).catch(err => console.log(err))
-    }
+  getAll () {
+    return this._connection.manager.find(Estado).catch(err => console.log(err))
+  }
+
+  getById (id:number) {
+    return this._connection.manager.findOne(Estado, id).catch(err => console.log(err))
+  }
 }
